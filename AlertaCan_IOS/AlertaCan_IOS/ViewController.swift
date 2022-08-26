cb166c120458d1039c53fc9b0cdc53748d2bf00b
Carlos Ruiz <carlos@Carloss-MacBook-Air.local>
2022-08-26 16:46:31 -0500

    Adding XCode files

---
+++ AlertaCan_IOS/AlertaCan_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertaCan_IOS
+//
+//  Created by Carlos Ruiz on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
