99618dded8f5cae7712be1ef15aee17b04b1fac4
Carlos Vega <A01731416@tec.mx>
2022-09-19 17:21:35 -0500

    [MAP] Display map with current users location

--- AlertaCan_IOS/AlertaCan_IOS/ViewController.swift
+++ AlertaCan_IOS/AlertaCan_IOS/ViewController.swift
@@ -5,15 +5,71 @@
 //  Created by Carlos Ruiz on 26/08/22.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+// Include CLLocationManagerDelegate to allow manager.delegatte in viewDidAppear
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // Map Kit Map View to create it
+    // By itself only shows current location
+    @IBOutlet var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    // Add a info.plist for the permission of current location
+    // Add a privacy
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Implications on battery
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        manager.delegate = self
+        // Request permissions from Info.plist
+        manager.requestWhenInUseAuthorization()
+        
+        manager.startUpdatingLocation()
+    }
 
+    // It is called when manager.startUpdatingLocation()
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // The manager starts storing them in the array locations and once the first one is found, it stops
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    
+    // Function to zoom in to the location and create a marker
+    func render(_ location: CLLocation) {
+        
+        // ------ CREATE MAP ------
+        // Get the coordinates according to the location
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        // By how much zoom the span will render it
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        // Creating the region that will be displayed
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        
+        // ------ CREATE PIN ------
+        // Create the object
+        let pin = MKPointAnnotation()
+        // Assign its coordinates
+        pin.coordinate = coordinate
+        // Add it to the mapView
+        mapView.addAnnotation(pin)
+    }
 
 }
+
 
