d7761d920058d633ce466745c4c925de1d30dbbb
JAngelGC <a01552274@tec.mx>
2022-10-01 11:00:39 -0500

    Activity_dog_info_updated

--- AlertaCan_Android/app/src/main/java/com/example/alertacan_android/MyDogInfoActivity.java
+++ AlertaCan_Android/app/src/main/java/com/example/alertacan_android/MyDogInfoActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DogInfoActivity extends AppCompatActivity {
+public class MyDogInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dog_info);
+        setContentView(R.layout.activity_my_dog_info);
     }
 }
