2e7960c92238a85afac9d2858b48ff40a1c281d6
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2022-10-02 00:39:39 -0500

    Implemented Login Authentication

--- AlertaCan_Android/app/src/main/java/com/example/alertacan_android/HomeActivity.java
+++ AlertaCan_Android/app/src/main/java/com/example/alertacan_android/HomeActivity.java
@@ -3,12 +3,23 @@ package com.example.alertacan_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Toast.makeText(HomeActivity.this, mAuth.getCurrentUser().toString(),
+                Toast.LENGTH_SHORT).show();
+
     }
 }
