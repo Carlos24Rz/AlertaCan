c42a9e2e594b45ff86201a8bd739a75d216f4a40
JosuAlejandroAl <A01732454@tec.mx>
2022-10-09 22:53:05 -0500

    [Login-SingUp] Navegación de pantallas, constrains y diseño [Controllers] Pantalla de SingUp y Login

---
+++ AlertaCan_IOS/AlertaCan_IOS/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  IOS_AlertaCan
+//
+//  Created by Josué Taboada Elvira on 09/10/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ AlertaCan_IOS/AlertaCan_IOS/SingUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SingUpViewController.swift
+//  IOS_AlertaCan
+//
+//  Created by Josué Taboada Elvira on 09/10/22.
+//
+
+import UIKit
+
+class SingUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
