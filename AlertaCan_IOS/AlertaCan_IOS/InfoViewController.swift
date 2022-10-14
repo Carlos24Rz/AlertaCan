28ddc213d515ecee29554b9243da3b8cec9b65bf
JosuAlejandroAl <A01732454@tec.mx>
2022-10-14 13:42:04 -0500

    [Info_Dog_Screen] Constrains Advance

---
+++ AlertaCan_IOS/AlertaCan_IOS/InfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoViewController.swift
+//  IOS_AlertaCan
+//
+//  Created by Josué Taboada Elvira on 13/10/22.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ AlertaCan_IOS/AlertaCan_IOS/MyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyViewController.swift
+//  IOS_AlertaCan
+//
+//  Created by Josué Taboada Elvira on 14/10/22.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
